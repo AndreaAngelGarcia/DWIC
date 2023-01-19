a6fb332b9d32e8075f45887ceb0e9c7a8d2ce8c5
Andrea Ángel García <andrea.angel@adaits.es>
2023-01-19 13:17:20 +0100

    Actualización

--- TAILWIND/web-replica/tailwind.config.js
+++ TAILWIND/web-replica/tailwind.config.js
@@ -1,25 +1,37 @@
-/* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./components//.{html,js}',
     './pages/**/.{html,js}',
     './index.html',],
+    variants: {
+      extend: {
+          animation:['responsive', 'hover', 'group-hover'],
+          fontSize:['responsive', 'hover', 'group-hover'],
+          transform:['responsive', 'hover', 'group-hover'],
+          scale:['responsive', 'hover', 'group-hover']
+      }
+    },
   theme: {
     extend: {
+      keyframes: {
+        rotateGear: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        fromBellow: {
+          '0%': { transform: 'translateY(0px)' },
+          '50%': {transform: 'translateY(56px)'},
+          '100%': { transform: 'translateY(0px)' },
+        }
+      },
       colors: {
         "azulClaro": "#37bcf9"
       },
       animation: {
-
+        'rotate-gear': 'rotateGear ',
+        'from-bellow': 'fromBellow 500ms linear',
       }
     },
-  },
-  variants: {
-    extend: {
-        animation:['responsive', 'hover', 'group-hover'],
-        fontSize:['responsive', 'hover', 'group-hover'],
-        transform:['responsive', 'hover', 'group-hover'],
-        scale:['responsive', 'hover', 'group-hover']
-    }
-  }
+  }  
 }
 
