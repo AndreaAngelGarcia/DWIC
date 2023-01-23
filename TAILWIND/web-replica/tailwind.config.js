a6b2b82c3503b33e49f1a4fe2aff92926570da03
Andrea Ángel García <andrea.angel@adaits.es>
2023-01-23 18:54:48 +0100

    actualización casa

--- TAILWIND/web-replica/tailwind.config.js
+++ TAILWIND/web-replica/tailwind.config.js
@@ -18,11 +18,20 @@ module.exports = {
           '0%': { transform: 'translateY(0px)'  },
           '50%': {transform: 'translateY(56px)' },
           '100%': { transform: 'translateY(0px)' },
+        },
+        rotateGear: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }, 
+        fromRight: {
+          '0%': { transform: 'translateX(56px)'  },
+          '100%': { transform: 'translateX(0px)' },
         }
       },
       animation: {
         'spin-slow': 'spin 2s linear infinite',
         'from-bellow': 'fromBellow 500ms linear',
+        'from-right': 'fromRight 300ms linear'
       },
       colors: {
         "azulClaro": "#37bcf9"
